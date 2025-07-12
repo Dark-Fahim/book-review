a92553a05c92865a450932a4a28849c29e32cfe6
Dark-Fahim <itsmunna1066@gmail.com>
2025-07-13 01:25:44 +0600

    navbar added

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
 
 
 const Root = () => {
     return (
         <div>
+            <Header></Header>
             <h2>this is root</h2>
             <Outlet></Outlet>
         </div>
