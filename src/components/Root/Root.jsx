8e637bccee6652ac0b8aa5b567981e89a2c9ae2b
Dark-Fahim <itsmunna1066@gmail.com>
2025-07-13 01:00:20 +0600

    project setup

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+
+const Root = () => {
+    return (
+        <div>
+            <h2>this is root</h2>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App.jsx'
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
